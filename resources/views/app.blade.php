4bf103e4d18565b94982ded818dc6784041c1a12
catvAlbuss <programdesing.rizabalasociados@gmail.com>
2026-07-02 09:51:14 -0500

    Actualizaciones de logos

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -31,9 +31,9 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/logo.ico" sizes="any">
+        <link rel="icon" href="/logo.svg" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/logo.png">
 
         @fonts
 
